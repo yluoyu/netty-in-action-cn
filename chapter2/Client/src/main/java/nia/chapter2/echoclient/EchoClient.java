d45dce1a15a1864337ddeadf42cec28bcba42155
yangwenchang <yangwenchang@nonobank.com>
2018-02-22 16:45:24 +0800

    修改验证

--- chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
+++ chapter2/Client/src/main/java/nia/chapter2/echoclient/EchoClient.java
@@ -57,15 +57,18 @@ public class EchoClient {
 
     public static void main(String[] args)
             throws Exception {
-        if (args.length != 2) {
+        //if (args.length != 2) {
+        if (false) {
             System.err.println("Usage: " + EchoClient.class.getSimpleName() +
                     " <host> <port>"
             );
             return;
         }
 
-        final String host = args[0];
-        final int port = Integer.parseInt(args[1]);
+        //final String host = args[0];
+        final String host = "127.0.0.1";
+        //final int port = Integer.parseInt(args[1]);
+        final int port = 9527;
         new EchoClient(host, port).start();
     }
 }
--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
@@ -24,14 +24,16 @@ public class EchoServer {
 
     public static void main(String[] args)
         throws Exception {
-        if (args.length != 1) {
+        //if (args.length != 1) {
+        if (false) {
             System.err.println("Usage: " + EchoServer.class.getSimpleName() +
                 " <port>"
             );
             return;
         }
         //设置端口值（如果端口参数的格式不正确，则抛出一个NumberFormatException）
-        int port = Integer.parseInt(args[0]);
+        int port = 9527;
+        //int port = Integer.parseInt(args[0]);
         //调用服务器的 start()方法
         new EchoServer(port).start();
     }
